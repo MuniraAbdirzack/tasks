4153a0b82fc6861553c7c11fe1dad7de4e9c9f3c
taskify1 <munira@gmail.com>
2024-11-06 02:43:33 -0800

    deleteTask

--- controllers/taskControllers.js
+++ controllers/taskControllers.js
@@ -39,14 +39,26 @@ exports.createTasks = (req, res) =>{
 
 }
 
-exports.updateTask = (req, res) => {
-    const form = new IncomingForm();
-    form.parse(req, (err, fields, files) => {
-        if(err) {
-            res.writeHead(400, { 'content-type': 'application/json'});
-            res.end(JSON.stringify({
-                message: 'Error parsing form'
-            }))
-            return;
-        }})
+
+
+exports.deleteTask = (req, res) => {
+    const tasks = readTasksFromFile();
+    const taskId = parseInt(req.url.split('/').pop());
+    const taskIndex = tasks.findIndex(task => task.id === taskId);
+
+    if (taskIndex === -1) {
+        res.writeHead(404, { 'content-type': 'application/json'});
+        res.end(JSON.stringify({
+            message: 'Task not found'
+        }))
+        return;
     }
+
+    const updatedTasks = tasks.filter(task => task.id !== taskId);
+    writeTasksToFile(updatedTasks);
+    res.writeHead(200, { 'content-type': 'application/json' });
+    res.end(JSON.stringify({
+        message: 'Task successfully deleted'
+    }));
+}
+
